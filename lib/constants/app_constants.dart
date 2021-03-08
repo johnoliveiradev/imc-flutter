181f9720cc2c8e78d4aa034a19bdf85ddb167d06
johnoliveiradev <johnoliveirayt@gmail.com>
2021-03-08 16:15:28 -0300

    first commit

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#110e29");
+final Color accentHexColor = HexColor("FCC91C");
